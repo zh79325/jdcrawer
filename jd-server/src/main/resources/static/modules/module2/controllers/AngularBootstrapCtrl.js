dc7681ad20c316903d71e783c8e84e8298e9061b
zh_zhou <zh_zhou@Ctrip.com>
2017-05-15 10:35:33 +0800

    add demo

---
+++ jd-server/src/main/resources/static/modules/module2/controllers/AngularBootstrapCtrl.js
@@ -0,0 +1,5 @@
+define(['app'], function (app) {
+    app.controller('AngularBootstrapCtrl', function ($scope, $http) {
+        $scope.data = 111111111;
+    });
+});
--- jd-server/src/main/resources/static/modules/module2/routers.js
+++ jd-server/src/main/resources/static/modules/module2/routers.js
@@ -5,15 +5,20 @@ define(['modules/module2/require-config'], function (config) {
       "tpl-folder": "modules/module2/views",
       "routers": [{
          "name": "首页",
-         "url": "/index2",
+         "url": "/demo",
          "controller": "Module2IndexCtrl",
-         "template": "index.html",
+         "template": "demo.html",
          "views": {
             "test": {
                "controller": "index/Module2IndexTestCtrl",
                "template": "index/test.html"
             }
          }
-      }]
+      },
+          {
+             url:"/ui-bootstrap-demo",
+              controller:"AngularBootstrapCtrl",
+              template:"angular-ui-bootstrap-demo.html"
+          }]
    }
 })
