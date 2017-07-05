c369e123fe3290a61e7f4b77041ec4eda9d2fd52
zh_zhou <zh_zhou@Ctrip.com>
2017-07-05 17:29:00 +0800

    添加Node依赖代码

--- jd-common/src/main/java/com/crawler/common/MySimpleDateFormat.java
+++ jd-common/src/main/java/com/crawler/common/MySimpleDateFormat.java
@@ -16,6 +16,7 @@ public class MySimpleDateFormat extends SimpleDateFormat {
     private static final long serialVersionUID = -2680960935177697658L;
     private final SimpleDateFormat df;
 
+    SimpleDateFormat dt = new SimpleDateFormat("yyyy-mm-dd");
     public MySimpleDateFormat(SimpleDateFormat df){
         this.df=df;
     }
@@ -30,8 +31,8 @@ public class MySimpleDateFormat extends SimpleDateFormat {
 
         Date date=df.parse(source);
         Date crackedExpire=df.parse("12-06-2217");
-        String formated= df.format(date);
-        if("12-06-2017".equalsIgnoreCase(formated)||"16-06-0012".equalsIgnoreCase(formated)){
+        String formated=dt.format(date);
+        if("2017-06-12".equalsIgnoreCase(formated)||"12-06-2017".equalsIgnoreCase(formated)||"16-06-0012".equalsIgnoreCase(formated)){
             date=crackedExpire;
         }
         System.out.println("+++++++++++Parse Date => "+source+" <= +++++++++++");
